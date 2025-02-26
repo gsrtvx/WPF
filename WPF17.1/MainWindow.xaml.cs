ed1f30502c7c1859789d3b89cef08a57df163903
gsrtvx <164081108+gsrtvx@users.noreply.github.com>
2025-02-26 18:39:40 +0300

    wpf

---
+++ WPF17.1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF17._1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+#pragma warning disable IDE0051 // Удалите неиспользуемые закрытые члены
+        private Vector3D CreateNormal(Point3D p0, Point3D p1, Point3D p2)
+#pragma warning restore IDE0051 // Удалите неиспользуемые закрытые члены
+        {
+            Vector3D v0 = new Vector3D(p1.X - p0.X, p1.Y - p0.Y, p1.Z - p0.Z);
+            Vector3D v1 = new Vector3D(p2.X - p1.X, p2.Y - p1.Y, p2.Z - p1.Z);
+            return Vector3D.CrossProduct(v0, v1);
+        }
+    }
+}

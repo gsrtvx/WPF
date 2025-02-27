c5aad66c0078b77aae1077da80b66cd3e97c9084
gsrtvx <164081108+gsrtvx@users.noreply.github.com>
2025-02-27 19:20:54 +0300

    Add files via upload

---
+++ WPF20/ApplicationViewModelBase.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPF20
+{
+    public class ApplicationViewModelBase
+    {
+
+        // команда добавления нового объекта
+        private RelayCommand addCommand;
+        private Phone selectedPhone;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                      Phone phone = new Phone();
+                      Phones.Insert(0, phone);
+                      SelectedPhone = phone;
+                  }));
+            }
+        }
+        public ObservableCollection<Phone> Phones { get; set; }
+
+        public Phone SelectedPhone
+        {
+            get { return selectedPhone; }
+            set
+            {
+                selectedPhone = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
